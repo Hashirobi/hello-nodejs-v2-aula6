eff1350f12133d710e715b26c1aa251232f1104f
Hashirobi <Luiz_Vitor7@outlook.com>
2016-11-30 20:19:14 -0300

    exercise 6

---
+++ appweb/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(bodyParser.json());
+
+app.post("/dosave",(req, res) => {
+	console.log("os dados recebidos do formulário são:");	
+	console.log(req.body);
+	res.send("<h1>Sucesso!</h1><a href='index.html'>voltar</a>");
+});
+app.post("/adivinha",(req, res)=> {
+	console.log("o(s) numeros foram recebidos:");	
+	console.log(req.body);
+	res.send("<h1>Sucesso!</h1><a href='numero.html'>voltar</a>");
+});
+app.get("/convidados", (req, res) => {
+	var convs = [{nomeconvidado:"Maria"}, {nomeconvidado:"João"}];
+	res.send(convs);
+});
+
+app.listen(3000);
+console.log("app online!");
+
